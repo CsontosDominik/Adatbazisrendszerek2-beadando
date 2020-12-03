9c79743864a101c94d2bc76e9bb422287f7b1472
CsontosDominik <61829086+CsontosDominik@users.noreply.github.com>
2020-12-03 15:02:23 +0100

    Records Ház_Építése property

---
+++ Adatbázissrendszerek 2 beadandó/Adatbázissrendszerek 2 beadandó/Models/Records/Haz.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adatbázissrendszerek_2_beadandó.Models.Records
+{
+    class Haz
+    {
+
+        private string epitese;
+        public string Epitese
+        {
+            get
+            {
+                return epitese;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Kérem adjon meg egy értéket a ház építéséhez.");
+                }
+                epitese = value;
+            }
+        }
+
+        // Építése pl tégla 
+        // Szobák száma pl 2
+        // Emelet pl 1
+        // Fűtés pl padló
+        // Utca ház szám 
+        // Város
+        // Irányítószám
+        // Típus Enum Kertes Társas
+
+    }
+}
